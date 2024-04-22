28e8eaa060f10116aa6b24ea151f0da2383428f5
Dhananjhay <dhananjhay03@gmail.com>
2024-04-22 16:41:28 +0530

    Will use githuh pages to render the website therefore deleted backend architecture

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
+import React from 'react';
+
 import './App.css';
+import PersonIcon from '@mui/icons-material/Person';
 
 function App() {
   return (
-    <>
-    <h3>Hello World!</h3>
-    </>
+    <React.Fragment>
+    <div className='header'>
+    Bansal Properties  
+
+    Login <PersonIcon/>
+    </div>
+    <div>
+      About
+      <img src="./images/pp.png" alt='pp' width='25%' height='25%'/>
+
+    </div>
+    <div className='footer'>
+      <section>
+        Residential
+      </section>
+      <section>
+        Commercial
+      </section>
+    </div>
+    </React.Fragment>
   );
 }
 
